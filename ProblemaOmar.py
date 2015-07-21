a3641c713426ab0de4ffc3ce3a3503ad43deb3f8
Alberto Ramirez <arawille@localhost.localdomain>
2015-07-20 22:17:13 -0500

    Inicio del codigo

---
+++ ProblemaOmar.py
@@ -0,0 +1,18 @@
+import math
+
+class Problemas:
+
+	def __init__(self):
+		self.var = [100000, 1777, 1855, 10**8, 3**37]
+
+	def Problema1(self):
+		pass
+
+	def Problema2(self):
+		pass
+	
+	def Problema3(self):
+		pass
+
+	def Problema4(self):
+		pass
